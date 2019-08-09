ffd30d0c6345bcb6ee684db4a688b955b087a743
Kevin Ros <kevinros1190@gmail.com>
2019-08-09 13:38:49 -0400

    changed comment

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -9,5 +9,5 @@ if [[ -n "$1" ]] ; then
 fi
 OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=$PORT -Djava.rmi.server.hostname=$IP -Dcom.sun.management.jmxremote.rmi.port=$PORT"
 
-#java $OPTS -Xmx4G -jar askme-nlp.jar
+#java $OPTS -Xmx4G -jar askme-web.jar
 java -Xmx4G -jar askme-web.jar
