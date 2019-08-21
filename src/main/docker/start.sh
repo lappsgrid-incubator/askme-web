a3ab642a871cce72952267776b95651001238c5b
Keith Suderman <suderman@cs.vassar.edu>
2019-08-21 13:40:55 -0500

    Change jar name

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -10,4 +10,4 @@ fi
 OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.port=$PORT -Djava.rmi.server.hostname=$IP -Dcom.sun.management.jmxremote.rmi.port=$PORT"
 
 #java $OPTS -Xmx4G -jar askme-web.jar
-java -Xmx4G -jar askme-web.jar
+java -Xmx4G -jar service.jar
