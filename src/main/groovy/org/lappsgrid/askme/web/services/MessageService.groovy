fe8028861d32a71c6f53f9cfd4787670aeff26c7
Keith Suderman <suderman@cs.vassar.edu>
2020-06-16 14:41:32 -0400

    Trying to fix conflicts

--- src/main/groovy/org/lappsgrid/askme/web/services/MessageService.groovy
+++ src/main/groovy/org/lappsgrid/askme/web/services/MessageService.groovy
@@ -11,8 +11,11 @@ import org.springframework.stereotype.Service
 class MessageService {
 
     private static final String[] DEFAULT_MESSAGE = [
-            '<h2>June 15, 2020 12:00 UTC</h2>',
-            'The Solr index is being updated and queries may return inconsistent, or even empty results. This message will be updated when the update has been completed.'
+            '<h2>June 16, 2020 12:09 UTC</h2>',
+            '<p>The index has been updated to include 64,668 documents from the <a href="https://ai2-semanticscholar-cord-19.s3-us-west-2.amazonaws.com/historical_releases.html">June 12 dataset</a>.</p>',
+            '<p><strong>Coming Soon</strong> Daily updates to the index.</p>'
+//            '<h2>June 15, 2020 12:00 UTC</h2>',
+//            'The Solr index is being updated and queries may return inconsistent, or even empty results. This message will be updated when the update has been completed.'
 //            '<h2>June 15, 2020 12:00 UTC</h2>',
 //            '<p>The index has been updated to include access to the <a href="https://www.semanticscholar.org/cord19/download">June 12 CORD-19 dataset</a>.</p>',
 //            '<p>The AskMe service is under constant development and may go down for maintenance without warning. We apologize for any inconvenience this may cause.</p>'
